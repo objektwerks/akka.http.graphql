a25f73b9d0c755d139c84a35abd598a70d6a985e
objektwerks <objektwerks@runbox.com>
2020-03-22 19:29:39 -0400

    edited integration test

--- jvm/src/it/scala/tripletail/IntegrationTest.scala
+++ jvm/src/it/scala/tripletail/IntegrationTest.scala
@@ -17,7 +17,7 @@ import scala.language.postfixOps
 class IntegrationTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
   val logger = LoggerFactory.getLogger(getClass)
   val conf = ConfigFactory.load("test.server.conf")
-  val actorRefFactory = ActorSystem.create(conf.getString("server.name"), conf.getConfig("akka"))
+  implicit val actorRefFactory = ActorSystem.create(conf.getString("server.name"), conf.getConfig("akka"))
   implicit val dispatcher = system.dispatcher
   implicit val timeout = RouteTestTimeout(10.seconds dilated)
 
