d8f6f24b1f7318c9f298f55baeb9b25a9c723361
objektwerks <objektwerks@runbox.com>
2020-04-06 07:39:18 -0400

    finished LicenseeStoreTest, which won't run with no jsom support for indexeddb

--- js/src/test/scala/tripletail/LicenseeStoreTest.scala
+++ js/src/test/scala/tripletail/LicenseeStoreTest.scala
@@ -10,15 +10,15 @@ object LicenseeStoreTest extends TestSuite {
       val licensee = Licensee(emailAddress = "tripletailwerks@gmail.com")
       for {
         optionalLicensee <- store.putLicensee(licensee)
-      } yield validate(optionalLicensee)
+      } yield validate(licensee, optionalLicensee)
       for {
         optionalLicensee <- store.getLicensee
-      } yield validate(optionalLicensee)
+      } yield validate(licensee, optionalLicensee)
     }
   }
 
-  private def validate(licensee: Option[Licensee]): Unit = licensee match {
-    case Some(licensee) => println(s"Licensee: $licensee")
-    case None => println(s"No Licensee!")
+  private def validate(licensee: Licensee, optionalLicensee: Option[Licensee]): Unit = optionalLicensee match {
+    case Some(value) => println(s"Licensee: $value"); assert(value == licensee)
+    case None => println(s"No Licensee!"); assert(false)
   }
 }
