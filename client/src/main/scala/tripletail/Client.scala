28536a824090eedd191fb4c36fbd3f14caf8973c
objektwerks <objektwerks@runbox.com>
2020-06-12 10:24:39 -0400

    implemented base JFXApp in Client

--- client/src/main/scala/tripletail/Client.scala
+++ client/src/main/scala/tripletail/Client.scala
@@ -1,7 +1,23 @@
 package tripletail
 
-object Client {
-  def main(args: Array[String]): Unit = {
-    println("Client main!")
+import scalafx.application.JFXApp
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.layout.VBox
+
+object Client extends JFXApp {
+  val appPane = new VBox {
+    maxWidth = 600
+    maxHeight = 600
+    spacing = 6
+    padding = Insets(6)
+    children = List()
+  }
+
+  stage = new JFXApp.PrimaryStage {
+    title.value = "Tripletail"
+    scene = new Scene {
+      root = appPane
+    }
   }
 }
