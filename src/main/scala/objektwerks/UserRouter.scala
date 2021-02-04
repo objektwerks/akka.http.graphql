1b0befa00e4ae5d52de6ee731550daa87b252e86
objektwerks <objektwerks@runbox.com>
2021-02-04 09:01:40 -0500

    added resource route

--- src/main/scala/objektwerks/UserRouter.scala
+++ src/main/scala/objektwerks/UserRouter.scala
@@ -1,5 +1,10 @@
 package objektwerks
 
-object UserRouter {
-  
+import akka.http.scaladsl.server.Directives
+
+trait UserRouter  extends Directives with JsonSupport {
+  val resources = get {
+    getFromResourceDirectory("graphql.html")
+  }
+  val routes = resources
 }
