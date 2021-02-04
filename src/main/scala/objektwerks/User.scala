4da88fc4c927d97d46b2dc85359976cdca222a82
objektwerks <objektwerks@runbox.com>
2021-02-04 10:09:05 -0500

    added graphql support to user

--- src/main/scala/objektwerks/User.scala
+++ src/main/scala/objektwerks/User.scala
@@ -2,9 +2,27 @@ package objektwerks
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
+import sangria.macros.derive._
+import sangria.schema._
 import spray.json._
 
-final case class User(id: Int, name: String)
+trait Identifiable {
+  def id: Int
+}
+
+final case class User(id: Int, name: String) extends Identifiable
+
+trait IdentifiableType {
+  implicit val identifiableType = InterfaceType(
+    "Identifiable",
+    "Entity that contains an id field.",
+    fields[Unit, Identifiable]( Field("id", IntType, resolve = _.value.id) )
+  )
+}
+
+trait UserType extends IdentifiableType {
+  implicit val userType = deriveObjectType[Unit, User]( Interfaces(identifiableType) )
+}
 
 trait UserJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val userFormat = jsonFormat2(User)
--- src/main/scala/objektwerks/UserRouter.scala
+++ src/main/scala/objektwerks/UserRouter.scala
@@ -2,7 +2,7 @@ package objektwerks
 
 import akka.http.scaladsl.server.Directives
 
-trait UserRouter  extends Directives with UserJsonSupport {
+trait UserRouter  extends Directives with UserType with UserJsonSupport {
   val index = path("") {
     getFromResource("user/graphql.html")
   }
