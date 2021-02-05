4982544eb32eeaf4501b1a0166720fe54e5b03d5
objektwerks <objektwerks@runbox.com>
2021-02-05 14:34:21 -0500

    extracted UserSchema

--- src/main/scala/objektwerks/User.scala
+++ src/main/scala/objektwerks/User.scala
@@ -2,9 +2,6 @@ package objektwerks
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 
-import sangria.macros.derive._
-import sangria.schema._
-
 import spray.json._
 
 trait Identifiable extends Product with Serializable {
@@ -12,35 +9,6 @@ trait Identifiable extends Product with Serializable {
 }
 
 case class User(id: Int, name: String) extends Identifiable
-
-trait UserSchema {
-  implicit val IdentifiableType = InterfaceType(
-    "Identifiable",
-    "Entity with id field.",
-    fields[Unit, Identifiable]( Field("id", IntType, resolve = _.value.id) )
-  )
-
-  implicit val UserType = deriveObjectType[Unit, User]( Interfaces( IdentifiableType ) )
-
-  val Id = Argument("id", IntType)
-
-  val UserQueryType = ObjectType("Query", fields[UserStore, Unit](
-    Field("find",
-      OptionType(UserType),
-      description = Some("Returns user by id."),
-      arguments = Id :: Nil,
-      resolve = context => context.ctx.find(context arg Id)
-    ),
-
-    Field("list",
-      ListType(UserType),
-      description = Some("Returns ist of users."),
-      resolve = context => context.ctx.list)
-    )
-  )
-
-  val UserSchema = Schema(UserQueryType)
-}
 
 trait UserJsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val userFormat = jsonFormat2(User)
---
+++ src/main/scala/objektwerks/UserSchema.scala
@@ -0,0 +1,33 @@
+package objektwerks
+
+import sangria.macros.derive.{Interfaces, deriveObjectType}
+import sangria.schema.{Argument, Field, IntType, InterfaceType, ListType, ObjectType, OptionType, Schema, fields}
+
+trait UserSchema {
+  implicit val IdentifiableType = InterfaceType(
+    "Identifiable",
+    "Entity with id field.",
+    fields[Unit, Identifiable]( Field("id", IntType, resolve = _.value.id) )
+  )
+
+  implicit val UserType = deriveObjectType[Unit, User]( Interfaces( IdentifiableType ) )
+
+  val Id = Argument("id", IntType)
+
+  val UserQueryType = ObjectType("Query", fields[UserStore, Unit](
+    Field("find",
+      OptionType(UserType),
+      description = Some("Returns user by id."),
+      arguments = Id :: Nil,
+      resolve = context => context.ctx.find(context arg Id)
+    ),
+
+    Field("list",
+      ListType(UserType),
+      description = Some("Returns ist of users."),
+      resolve = context => context.ctx.list)
+    )
+  )
+
+  val UserSchema = Schema(UserQueryType)
+}
