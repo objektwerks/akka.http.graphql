7d5d59084fffc3124aa4e077756c722cb3caa15a
objektwerks <objektwerks@runbox.com>
2021-02-09 10:27:24 -0500

    enhanced user app test

--- src/test/scala/objektwerks/UserAppTest.scala
+++ src/test/scala/objektwerks/UserAppTest.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
@@ -14,6 +13,7 @@ import spray.json.JsValue
 
 class UserAppTest extends AnyWordSpec with Matchers with ScalatestRouteTest with BeforeAndAfterAll {
   import TestConf._
+  import UserJsonSupport._
 
   val actorRefFactory = ActorSystem.create(name, conf)
 
@@ -40,13 +40,17 @@ class UserAppTest extends AnyWordSpec with Matchers with ScalatestRouteTest with
   "list" should {
     "list users" in {
       val response = """{"data":{"list":[{"id":1,"name":"Fred Flintstone"},{"id":2,"name":"Barney Rebel"}]}}"""
+      val validateResponse = (json: String) => {
+        json shouldBe response
+        jsonToUsers( json ) shouldBe Seq(User(1, "Fred Flintstone"), User(2, "Barney Rebel"))
+      }
       Get("/graphql", UserQueries.listQueryAsJsValue) ~> routes ~> check {
         status shouldBe StatusCodes.OK
-        responseAs[JsValue].compactPrint shouldBe response
+        validateResponse( responseAs[JsValue].compactPrint )
       }
       Post("/graphql", UserQueries.listQueryAsJsValue) ~> routes ~> check {
         status shouldBe StatusCodes.OK
-        responseAs[JsValue].compactPrint shouldBe response
+        validateResponse( responseAs[JsValue].compactPrint )
       }
     }
   }
@@ -54,13 +58,17 @@ class UserAppTest extends AnyWordSpec with Matchers with ScalatestRouteTest with
   "find" should {
     "find a user" in {
       val response = """{"data":{"find":{"id":1,"name":"Fred Flintstone"}}}"""
+      val validateResponse = (json: String) => {
+        json shouldBe response
+        jsonToUser( json ) shouldBe User(1, "Fred Flintstone")
+      }
       Get("/graphql", UserQueries.findQueryAsJsValue) ~> routes ~> check {
         status shouldBe StatusCodes.OK
-        responseAs[JsValue].compactPrint shouldBe response
+        validateResponse( responseAs[JsValue].compactPrint )
       }
       Post("/graphql", UserQueries.findQueryAsJsValue) ~> routes ~> check {
         status shouldBe StatusCodes.OK
-        responseAs[JsValue].compactPrint shouldBe response
+        validateResponse( responseAs[JsValue].compactPrint )
       }
     }
   }
