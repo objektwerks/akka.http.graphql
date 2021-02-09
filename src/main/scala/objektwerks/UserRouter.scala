bc3d41f0d7dabce0c67bd52b01e4165ff9725c2f
objektwerks <objektwerks@runbox.com>
2021-02-09 16:13:36 -0500

    refactored user router, removing graphql

--- src/main/scala/objektwerks/UserRouter.scala
+++ src/main/scala/objektwerks/UserRouter.scala
@@ -1,23 +1,47 @@
 package objektwerks
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.StatusCode
 import akka.http.scaladsl.model.StatusCodes.BadRequest
 import akka.http.scaladsl.server.Directives
-
+import sangria.ast.Document
+import sangria.execution.ExecutionScheme.Default.Result
+import sangria.execution.Executor
+import sangria.marshalling.sprayJson.{SprayJsonInputUnmarshallerJObject, SprayJsonResultMarshaller}
+import sangria.parser.QueryParser
+import sangria.schema.Schema
 import spray.json.{JsObject, JsString, JsValue}
 
 import scala.concurrent.ExecutionContextExecutor
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 object UserRouter {
   def apply(userSchema: UserSchema, userStore: UserStore)
            (implicit executor: ExecutionContextExecutor): UserRouter = new UserRouter(userSchema, userStore)
+
+  def parseQueryJsValue(queryJsValue: JsValue): Try[(String, Option[String], JsObject)] = Try {
+    val JsObject(fields) = queryJsValue
+    val JsString(query) = fields("query")
+    val operationName = fields.get("operationName") collect { case JsString(op) => op }
+    val variables = fields.getOrElse("variables", JsObject.empty).asJsObject
+    (query, operationName, variables)
+  }
+
+  def parseQuery(query: String): Try[Document] = QueryParser.parse(query)
+
+  def executeQuery(userSchema: Schema[UserStore, Unit],
+                   userStore: UserStore,
+                   query: Document,
+                   operationName: Option[String],
+                   variables: JsObject)(implicit executor: ExecutionContextExecutor): Result[StatusCode, JsValue] =
+    Executor.execute(userSchema, query, userStore, variables = variables, operationName = operationName)
 }
 
 class UserRouter(userSchema: UserSchema, userStore: UserStore)
                 (implicit executor: ExecutionContextExecutor) extends Directives {
+  import UserRouter._
+
   val querySchema = userSchema.UserSchema
-  val toBadRequest = (error: Throwable) => complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
 
   val index = path("") {
     getFromResource("public/graphql.html")
@@ -26,14 +50,13 @@ class UserRouter(userSchema: UserSchema, userStore: UserStore)
   val api = path("graphql") {
     (get | post) {
       entity(as[JsValue]) { queryJsValue =>
-        import GraphQL._
-        parseQueryJsValue(queryJsValue) match {
-          case Success((query, operationName, variables)) =>
-            parseQuery(query) match {
-              case Success(document) => complete(executeQuery(querySchema, userStore, document, operationName, variables))
-              case Failure(error) => toBadRequest(error)
-            }
-          case Failure(error) => toBadRequest(error)
+        val futureResult = for {
+          (query, operationName, variables) <- parseQueryJsValue(queryJsValue)
+          document <- parseQuery(query)
+        } yield executeQuery(querySchema, userStore, document, operationName, variables)
+        futureResult match {
+          case Success(result) => complete(result)
+          case Failure(error) => complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
         }
       }
     }
