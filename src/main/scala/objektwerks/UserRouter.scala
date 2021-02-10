2bd871b56735a4fe57be846d08eac7b1cf6c7e65
objektwerks <objektwerks@runbox.com>
2021-02-10 10:17:08 -0500

    refactored sangria executor.execute call

--- src/main/scala/objektwerks/UserRouter.scala
+++ src/main/scala/objektwerks/UserRouter.scala
@@ -1,12 +1,9 @@
 package objektwerks
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCode
 import akka.http.scaladsl.model.StatusCodes.BadRequest
 import akka.http.scaladsl.server.Directives
 
-import sangria.ast.Document
-import sangria.execution.ExecutionScheme.Default.Result
 import sangria.execution.Executor
 import sangria.marshalling.sprayJson.{SprayJsonInputUnmarshallerJObject, SprayJsonResultMarshaller}
 import sangria.schema.Schema
@@ -17,21 +14,15 @@ import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 object UserRouter {
-  def apply(userSchema: Schema[UserStore, Unit], userStore: UserStore)
+  def apply(userSchema: Schema[UserStore, Unit],
+            userStore: UserStore)
            (implicit executor: ExecutionContextExecutor): UserRouter = new UserRouter(userSchema, userStore)
-
-  def executeQuery(userSchema: Schema[UserStore, Unit],
-                   userStore: UserStore,
-                   query: Document,
-                   operationName: Option[String],
-                   variables: JsObject)(implicit executor: ExecutionContextExecutor): Result[StatusCode, JsValue] =
-    Executor.execute(userSchema, query, userStore, variables = variables, operationName = operationName)
 }
 
-class UserRouter(userSchema: Schema[UserStore, Unit], userStore: UserStore)
+class UserRouter(userSchema: Schema[UserStore, Unit],
+                 userStore: UserStore)
                 (implicit executor: ExecutionContextExecutor) extends Directives {
   import GraphQL._
-  import UserRouter._
 
   val index = path("") {
     getFromResource("public/graphql.html")
@@ -43,7 +34,7 @@ class UserRouter(userSchema: Schema[UserStore, Unit], userStore: UserStore)
         val tryResult = for {
           (query, operationName, variables) <- parseQueryJsValue(queryJsValue)
           document <- parseQuery(query)
-        } yield executeQuery(userSchema, userStore, document, operationName, variables)
+        } yield Executor.execute(userSchema, document, userStore, variables = variables, operationName = operationName)
         tryResult match {
           case Success(result) => complete(result)
           case Failure(error) => complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
