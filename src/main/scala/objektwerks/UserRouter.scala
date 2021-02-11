20c0a8b575fa0b6f92a09cdfdff7e4bda3e9460e
objektwerks <objektwerks@runbox.com>
2021-02-11 08:31:49 -0500

    refactored user router

--- src/main/scala/objektwerks/UserRouter.scala
+++ src/main/scala/objektwerks/UserRouter.scala
@@ -1,10 +1,10 @@
 package objektwerks
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes.BadRequest
+import akka.http.scaladsl.model.StatusCodes.{BadRequest, InternalServerError, OK}
 import akka.http.scaladsl.server.Directives
 
-import sangria.execution.Executor
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
 import sangria.marshalling.sprayJson.{SprayJsonInputUnmarshallerJObject, SprayJsonResultMarshaller}
 import sangria.schema.Schema
 
@@ -29,12 +29,20 @@ class UserRouter(userSchema: Schema[UserStore, Unit], userStore: UserStore)
   val api = path("graphql") {
     (get | post) {
       entity(as[JsValue]) { queryJsValue =>
-        val result = for {
+        val parsedQueryArgs = for {
           (query, operationName, variables) <- parseQueryJsValue(queryJsValue)
           document <- parseQuery(query)
-        } yield Executor.execute(userSchema, document, userStore, variables = variables, operationName = operationName)
-        result match {
-          case Success(response) => complete(response)
+        } yield (document, operationName, variables)
+        parsedQueryArgs match {
+          case Success( (document, operationName, variables) ) =>
+            complete(
+              Executor.execute(userSchema, document, userStore, variables = variables, operationName = operationName)
+                .map( response => OK -> response )
+                .recover {
+                  case error: QueryAnalysisError => BadRequest -> error.resolveError
+                  case error: ErrorWithResolver => InternalServerError -> error.resolveError
+                }
+            )
           case Failure(error) => complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
         }
       }
